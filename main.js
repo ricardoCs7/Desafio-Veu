7531abfbc4e4310bd47525226446c9b88c6b2696
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-10-06 23:06:37 -0300

    edit not working

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ class Producto {
     this.nombre = producto.nombre || ""
     this.descripcion = producto.descripcion || ""
     this.precio = producto.precio || ""
-        this.status = producto.status || false
+    this.status = producto.status || false
   }
 }
 
 const app = Vue.createApp({
   data() {
     return {
+      editProducto: { editNombre :'', editDes:'' } ,
       newProducto: new Producto(),
-      productos: [new Producto({nombre: 'arya',descripcion: 'Gata Calico', precio: 1})],
+      productos: [new Producto({ nombre: 'Arya', descripcion: 'Gata Calico', precio: 100000 })],
       error: false,
       editIndex: -1,
     }
@@ -19,7 +20,7 @@ const app = Vue.createApp({
   methods: {
     addProducto() {
       this.error = false;
-      if (this.newProducto.nombre.length>0 & this.newProducto.descripcion.length>0 & this.newProducto.precio>0) {
+      if (this.newProducto.nombre.length > 0 & this.newProducto.descripcion.length > 0 & this.newProducto.precio > 0) {
         this.productos.push(this.newProducto);
         this.newProducto = new Producto()
       } else {
@@ -29,7 +30,24 @@ const app = Vue.createApp({
     deleteProducto(index) {
       const confirm = window.confirm('¿Estás seguro de eliminar el producto?');
       if (confirm) this.productos.splice(index, 1);
-    }
+    },
+    setProducto(index) {
+      this.editIndex = index;
+      this.editProducto = this.productos[index].nombre;
+      this.editProducto = this.productos[index].descripcion;
+      this.editProducto = this.productos[index].precio;
+
+      console.log(this.productos[index].nombre, this.productos[index].descripcion, this.productos[index].precio);
+    },
+    saveProducto(index) {
+      this.productos[index].nombre = this.editProducto;
+      
+     
+      this.editIndex = -1;
+    },
+    cancelar() {
+      this.editIndex = -1
+    },
   }
 })
 
